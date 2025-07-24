43088e19eae6aeace610095492bf3116728a5923
Johannes Roos <jhnnsrs@gmail.com>
2025-07-24 22:27:03 +0200

    refactor: organize imports and streamline structure registration

--- mikro_next/rekuest.py
+++ mikro_next/rekuest.py
@@ -3,7 +3,40 @@ from rekuest_next.structures.default import (
     id_shrink,
 )
 from rekuest_next.widgets import SearchWidget
-from mikro_next.api.schema import *
+from mikro_next.api.schema import (
+    Image,
+    Snapshot,
+    ROI,
+    Stage,
+    Dataset,
+    File,
+    RGBContext,
+    Mesh,
+    Table,
+    TableCell,
+    TableRow,
+    aget_image,
+    aget_snapshot,
+    aget_roi,
+    aget_stage,
+    aget_dataset,
+    aget_file,
+    aget_rgb_context,
+    aget_mesh,
+    aget_table,
+    aget_table_cell,
+    aget_table_row,
+    SearchImagesQuery,
+    SearchSnapshotsQuery,
+    SearchRoisQuery,
+    SearchStagesQuery,
+    SearchDatasetsQuery,
+    SearchFilesQuery,
+    SearchMeshesQuery,
+    SearchTablesQuery,
+    SearchTableCellsQuery,
+    SearchTableRowsQuery,
+)
 
 
 structure_reg = get_default_structure_registry()
@@ -80,9 +113,7 @@ structure_reg.register_as_structure(
     identifier="@mikro/tablecell",
     aexpand=aget_table_cell,
     ashrink=id_shrink,
-    default_widget=SearchWidget(
-        query=SearchTableCellsQuery.Meta.document, ward="mikro"
-    ),
+    default_widget=SearchWidget(query=SearchTableCellsQuery.Meta.document, ward="mikro"),
 )
 
 structure_reg.register_as_structure(
